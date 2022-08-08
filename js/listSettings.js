4b0c15147746288ec1e36357d1acc80dc8090bd1
99061142 <99061142@mydavinci.nl>
2022-08-08 23:09:11 +0200

    Added comments and changed function naming

--- js/listSettings.js
+++ js/listSettings.js
@@ -3,27 +3,32 @@ import { List } from './list.js';
 class ListSettings extends List {
     constructor() {
         super();
+        // Attributes
         this.sizeSlider = document.getElementById('listSize');
-        this.listSize = Number(this.sizeSlider.value);
         this.newArray = document.getElementById('newArray');
         this.algorithmsOptions = document.getElementById("algorithms");
         this.runButton = document.getElementById('run-button');
 
-        this.settingListeners();
-        this.setRandomList();
-
-        this.sizeSlider.max = window.innerWidth;
+        // Add event listeners
+        this.sizeSliderEvent();
+        this.newArrayButtonEvent();
+        this.runButtonEvent();
+        this.windowSizeEvent();
+        
+        // When page is loaded
+        this.setRandomList(); // Create random list
+        this.sizeSlider.max = window.innerWidth; // Set max size value to the width of the window
     }
 
     get listSizeValue() {
-        return this.listSize;
+        return Number(this.sizeSlider.value);
     }
 
     set listSizeValue(value) {
         this.listSize = Number(value);
     }    
 
-    listSizeListener() {
+    sizeSliderEvent() {
         // when user releases the slider
         this.sizeSlider.addEventListener('mouseup', () => {
             this.listSizeValue = this.sizeSlider.value;
@@ -31,37 +36,30 @@ class ListSettings extends List {
         });
     }
 
-    newArrayListener() {
+    newArrayButtonEvent() {
         // Generate new array
         this.newArray.addEventListener('click', () => {
-            this.listSizeValue = this.sizeSlider.value;
             this.setRandomList();
         });
     }
 
-    sortArrayListener() {
+    runButtonEvent() {
         // Run array    
         this.runButton.addEventListener('click', () => {
             this.run();
         });
     }
 
-    settingListeners() {
-        this.listSizeListener();
-        this.newArrayListener();
-        this.sortArrayListener();
-        this.windowSizeListener();
-    }
-
-    windowSizeListener() {
+    windowSizeEvent() {
         window.addEventListener('resize', () => {
-            this.sizeSlider.max = window.innerWidth;
-            this.resizeList(); // resize list length based on the new width of the screen
+            this.sizeSlider.max = window.innerWidth; // Set max size value to the width of the window
+            this.resizeList(); // Delete values out of list when window size is decreased
             this.resizeListValues(); // resize list values on the screen based on the new width of the screen
         });
     }
 
     toggleSettings() {
+        // Disable or enable setting attributes when run button is clicked
         this.sizeSlider.disabled = this.running;
         this.newArray.disabled = this.running;
         this.algorithmsOptions.disabled = this.running;
