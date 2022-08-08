a8e9f974928333385713f975ea8888277a818575
99061142 <99061142@mydavinci.nl>
2022-08-09 01:39:54 +0200

    Created constructor / run function without event

---
+++ js/algorithms/selectionSort.js
@@ -0,0 +1,12 @@
+export class SelectionSort {
+    constructor(list, callback) {
+        this.list = list;
+        this.callback = callback;
+    }
+
+
+    run() {
+
+        console.log(this.list);
+    }
+}
