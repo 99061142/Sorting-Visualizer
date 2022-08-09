6e321cf0ed2190ceff512165a4df6222942d9c9b
99061142 <99061142@mydavinci.nl>
2022-08-09 02:05:53 +0200

    Sort list with selection sort

--- js/algorithms/selectionSort.js
+++ js/algorithms/selectionSort.js
@@ -6,7 +6,31 @@ export class SelectionSort {
 
 
     run() {
+        // For every number in the list
+        for(let i = 0; i < this.list.length; i++) {
+            let currentNumber = this.list[i];
+            let currentIndex = i;
+            let smallestNumberIndex = i;
+            let smallestNumber = currentNumber;
 
-        console.log(this.list);
+            // For every number in the list after the current number
+            for(let j = i + 1; j < this.list.length; j++) {
+                let nextNumber = this.list[j];
+
+                // If the next number is smaller than the current number
+                if(nextNumber < smallestNumber) {
+                    smallestNumber = nextNumber;
+                    smallestNumberIndex = j;
+                }
+            }
+
+            // If the index of the current smallest number is not the same as the current index
+            if(smallestNumberIndex && currentIndex != smallestNumberIndex) {
+                // Swap the current number with the smallest number
+                this.list[currentIndex] = smallestNumber;
+                this.list[smallestNumberIndex] = currentNumber;
+            }
+        }
+        this.callback();
     }
 }
