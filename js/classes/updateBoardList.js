b3f313f4133f96a40ae120ff6a52fd4f666ad68b
99061142 <99061142@mydavinci.nl>
2022-08-09 12:48:23 +0200

    Made code more OOP and readable
    * Added clear board / clear board except green child functions.
    
    * Added comments

--- js/classes/updateBoardList.js
+++ js/classes/updateBoardList.js
@@ -1,11 +1,49 @@
 export class UpdateBoardList {
     constructor() {
+        this.colors = {
+            standard: 'blue',
+            next: 'orange',
+            found: 'green',
+            smallest: 'red'
+        }
+
         this.listDiv = document.getElementById('list');
+        this.speedRange = document.getElementById('listSortingSpeed')
         this.smallestIndex = null;
     }
 
+    clearBoard() {
+        for(let i = 0; i < this.children.length; i++) {
+            this.standard(i);
+        }
+    }
+
+    clearBoardExceptFound() {
+        for(let i = 0; i < this.listDiv.children.length; i++) {
+            if(!this.childFound(i)) { this.standard(i); }
+        }
+    }
+
+    setChildBackgroundColor(i, color) {
+        this.getChild(i).style.backgroundColor = color;
+    }
+
+    getChild(i) {
+        return this.listDiv.children.item(i);
+    }
+
+    getChildBackgroundColor(i) {
+        return this.getChild(i).style.backgroundColor;
+    }
+
+    childFound(i) {
+        return this.getChild(i).style.backgroundColor == this.colors['found'];
+    }
+
     sleep() {
-        let ms = document.getElementById('listSortingSpeed').value;
+        let max_speed = this.speedRange.max
+        let current = this.speedRange.value
+        let ms = max_speed - current;
         if(ms) { return new Promise(resolve => setTimeout(resolve, ms)); }
     }
 
@@ -13,17 +51,21 @@ export class UpdateBoardList {
         return this.listDiv.children;
     }
 
-    current(i) {
-        this.children.item(i).style.backgroundColor = 'green';
+    async found(i) {
+        await this.sleep();
+        let color = this.colors['found'];
+        this.setChildBackgroundColor(i, color);
     }
 
     async next(i) {
         await this.sleep();
-        this.children.item(i).style.backgroundColor = 'orange';
+        let color = this.colors['next'];
+        this.setChildBackgroundColor(i, color);
     }
     
     standard(i) {
-        this.children.item(i).style.backgroundColor = 'blue';
+        let color = this.colors['standard'];
+        this.setChildBackgroundColor(i, color);
     }
 
     isSmallest(i) {
@@ -31,10 +73,14 @@ export class UpdateBoardList {
     }
 
     smallest(i) { 
-        if(!this.isSmallest(i)) { this.next(this.smallestIndex); }
+        // Set child to smallest and save child value when the child value is smaller than the current smallest value
+        if(this.smallestIndex && !this.isSmallest(i)) { 
+            this.next(this.smallestIndex); 
+        }
         this.smallestIndex = i;
 
-        this.children.item(i).style.backgroundColor = 'red';
+        let color = this.colors['smallest'];
+        this.setChildBackgroundColor(i, color);
     }
 
     updateHeight(i, value) {
