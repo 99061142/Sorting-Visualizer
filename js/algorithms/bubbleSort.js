85356ae2a320e7582b72c06bbcf0e6548db4b332
99061142 <99061142@mydavinci.nl>
2022-08-11 01:27:09 +0200

    Starting bubble sort class

---
+++ js/algorithms/bubbleSort.js
@@ -0,0 +1,11 @@
+import { UpdateBoardList } from "../classes/updateBoardList.js"
+
+export class BubbleSort extends UpdateBoardList {
+    constructor(list) {
+        super();
+        this.list = list;
+    }
+
+    async run() {
+    }
+}
