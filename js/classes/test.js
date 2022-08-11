41f3cc036d4e5a986807f73038e41e4ae3ace229
99061142 <99061142@mydavinci.nl>
2022-08-11 02:50:44 +0200

    Set list size to 20 for tests to make tests faster

--- js/classes/test.js
+++ js/classes/test.js
@@ -24,24 +24,27 @@ export class Test {
     async algorithmTests() {
         // Save list setting before testing
         this.startingSpeed = this.speed.value;
-
         this.startingAlgorithm = this.algorithmOptions.value // Save original algorithm
+        this.startingListSize = this.sizeSlider.value
+
         this.speed.value = this.speed.max; // Set speed to max
+        this.sizeSlider.value = 20; // Set list size to 20
 
         // Test every algorithm that isn't disabled
         for(let algorithm of this.algorithmOptions) {
             if(algorithm.disabled) { continue; } // Skip disabled algorithms
             this.algorithmOptions.value = algorithm.value; // Set algorithm
+            this.updateList(); // Create new list
 
             // Run algorithm and compare to correct answer
             let answer = await this.run();
             this.compare(answer);
-
-            this.updateList(); // Create new list
         }
         // Reset list settings to original values
         this.speed.value = this.startingSpeed;
         this.algorithmOptions.value = this.startingAlgorithm;
+        this.sizeSlider.value = this.startingListSize;
+        this.updateList(); // Create new list
     }
 
     runTests() {
