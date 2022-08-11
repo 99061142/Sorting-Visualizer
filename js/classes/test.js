fddb741fda74dba977eb55cf61f6d8d775995fee
99061142 <99061142@mydavinci.nl>
2022-08-11 02:28:09 +0200

    Added error when list is empty / reset every list

--- js/classes/test.js
+++ js/classes/test.js
@@ -1,38 +1,35 @@
-import { ListSettings } from './ListSettings.js';
+import { List } from "./list.js";
 
-export class Test extends ListSettings {
+export class Test {
     constructor() {
-        super();
         this.testing = false;
+        this.speed = document.getElementById('listSortingSpeed')
     }
 
     compare(answer) {
         let correctList = [...this.list].sort((a, b) => a - b);
         let correct = answer.every((value, index) => value == correctList[index]); // Check if sorted correctly
-
-        if(correct) { return console.log(`Algorithm '${this.algorithm}' is working correctly`); }
-        console.warn(`Algorithm '${this.algorithm}' is not working correctly`);
+        
+        if(!answer.length) { return console.warn(`Algorithm '${this.algorithm}' anwer is empty`) }
+        if(!correct) { return console.warn(`Algorithm '${this.algorithm}' is not working correctly`); }
+        console.log(`Algorithm '${this.algorithm}' is working correctly`);
     }
 
     async runTests() {
         if(this.running) { return; } // Do not test algorithm if it's running
         this.testing = true;
-
-        let lastChosenAlgorithm = this.algorithmOptions.value; // Save last chosen algorithm
+        this.speed.value = this.speed.max
 
         // Test every algorithm that isn't disabled
         for(let algorithm of this.algorithmOptions) {
             if(algorithm.disabled) { continue; } // Skip disabled algorithms
+            this.algorithmOptions.value = algorithm.value; // Set algorithm
 
-            this.algorithmOptions.value = algorithm.value;
+            // Run algorithm and compare to correct answer
             let answer = await this.run();
             this.compare(answer);
+
+            this.updateList(); // Create new list
         }
-        this.algorithmOptions.value = lastChosenAlgorithm; // Restore last chosen algorithm
     }
 }
-let test = new Test();
-
-document.getElementById('test-algorithms').addEventListener('click', () => {
-    test.runTests();
-});
