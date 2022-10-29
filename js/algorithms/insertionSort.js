72dd360904951fadd83d00fcb302b0eb1306209f
99061142 <xarbprive@gmail.com>
2022-10-30 01:36:09 +0200

    Update variable / function naming
    Update variable / function naming and don't return sorted list.

--- js/algorithms/insertionSort.js
+++ js/algorithms/insertionSort.js
@@ -3,34 +3,38 @@ import { UpdateBoardList } from "../classes/updateBoardList.js"
 export class InsertionSort extends UpdateBoardList {
     constructor() {
         super();
+        this.lastSortedIndex = null;
     }
 
-    async smallest(current) {
-        // While the left element is smaller than the "current" element
-        while(current > 0 && this.previousSmallest(current)) {
-            // Set element left from current as "next"
-            if(current-1 >= 0) { 
-                await this.next(current-1); 
+    async loop(current) {
+        // Set current element as "sorted" when left element is smaller 
+        if(this.previousSmaller(current)) {
+            // Delete the sorted element from the board
+            if(this.lastSortedIndex != null) {
+                this.standard(this.lastSortedIndex);
             }
-            this.current(current); // Set current element as "current"
-            await this.switch(current, current-1); // Switch left and right element
-            
-            // Set element left from current and current element as "found"
-            await this.found(current);
-            this.found(current-1);
+            // Set current element to the board as "sorted"
+            this.sorted(current);
+            this.lastSortedIndex = current;
+        }
+
+        // While the left element is smaller than the current element
+        while(current > 0 && this.previousSmaller(current)) {
+            await this.next(current-1);
+            await this.switchHeight(current, current-1);
             current--;
         }
     }
 
     async run() {
         this.clearBoard();
-        this.found(0); // Always set the first element to found
+        this.next(0); // Set first element as "next"
 
         // For every number in the list
-        for(let i = 0; i < this.listSize; i++) {
-            await this.smallest(i); // Move highest number to the end of the list
+        for(let i = 0; i < this.elementsAmount; i++) {
+            await this.loop(i); // Move highest number to the end of the list
+            this.clearBoardExceptSorted(); // Clear board except elements that are already sorted
         }
-        await this.fullBoardFound(); // Set all elements to found
-        return this.numbers;
+        await this.fullBoardSorted(); // Set all elements to "sorted"
     }
 }
