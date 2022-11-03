b090b2d947c5b0003a47800571f4f6814087e8e4
99061142 <xarbprive@gmail.com>
2022-11-03 16:06:32 +0100

    Changed variable and function namings

--- js/algorithms/insertionSort.js
+++ js/algorithms/insertionSort.js
@@ -9,32 +9,32 @@ export class InsertionSort extends UpdateBoardList {
     async loop(current) {
         // Set current element as "sorted" when left element is smaller 
         if(this.previousSmaller(current)) {
-            // Delete the sorted element from the board
+            // Delete the last sorted element from the board
             if(this.lastSortedIndex != null) {
                 this.standard(this.lastSortedIndex);
             }
-            // Set current element to the board as "sorted"
+            // Set current element as "sorted"
             this.sorted(current);
             this.lastSortedIndex = current;
         }
 
-        // While the left element is smaller than the current element
+        // Swap current and left element height if left number is larger
         while(current > 0 && this.previousSmaller(current)) {
             await this.next(current-1);
-            await this.switchHeight(current, current-1);
+            await this.swapHeights(current, current-1);
             current--;
         }
     }
 
     async run() {
         this.clearBoard();
-        this.next(0); // Set first element as "next"
+        this.next(0);
 
         // For every number in the list
-        for(let i = 0; i < this.elementsAmount; i++) {
+        for(let i = 0; i < this.numbersAmount; i++) {
             await this.loop(i); // Move highest number to the end of the list
             this.clearBoardExceptSorted(); // Clear board except elements that are already sorted
         }
-        await this.fullBoardSorted(); // Set all elements to "sorted"
+        await this.fullBoardSorted();
     }
 }
