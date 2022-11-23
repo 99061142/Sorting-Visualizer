2ea61c582f00ed7ea25b22c355f78a24726a0d67
99061142 <xarbprive@gmail.com>
2022-11-23 13:59:38 +0100

    Don't update list size when algorithm is running

--- js/classes/settings.js
+++ js/classes/settings.js
@@ -2,13 +2,6 @@ import { Run } from './run.js';
 import { Test } from './test.js';
 import { Board } from './board.js';
 
-// Setting buttons
-let runButton = document.getElementById("runButton");
-let newArrayButton = document.getElementById("newArray");
-let sizeSlider = document.getElementById("sizeSlider");
-let algorithmOptions = document.getElementById("algorithms");
-let testButton = document.getElementById("testButton");
-
 // Classes
 let run = new Run(switchSettingsState);
 let test = new Test(switchSettingsState, resetBoard);
@@ -27,31 +20,38 @@ function resetBoard() {
 }
 
 // Run the algorithm
+let runButton = document.getElementById("runButton");
 runButton.addEventListener('click', () => {
     run.run();
 });
 
 // When the user clicks the new array button, create a new array
+let newArrayButton = document.getElementById("newArray");
 newArrayButton.addEventListener('click', () => {
     resetBoard();
 });
 
 // When the algorithm is changed, create a new array
+let algorithmOptions = document.getElementById("algorithms");
 algorithmOptions.addEventListener('change', () => {
     resetBoard();
 });
 
 // When the window is resized, update the board
 window.addEventListener('resize', () => {
+    // If the algorithm is running, return
+    if(run.running) { return; }
     board.resize();
 });
 
 // When the size is changed, update the board
+let sizeSlider = document.getElementById("sizeSlider");
 sizeSlider.addEventListener('input', () => {
     board.update();
 });
 
 // When the test button is clicked, run the test
+let testButton = document.getElementById("testButton");
 testButton.addEventListener('click', () => {
     test.test();
 });
