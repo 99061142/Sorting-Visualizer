d7b46ea3b3f07bbac782211d13239f184a3ec6f6
99061142 <xarbprive@gmail.com>
2023-02-27 20:13:18 +0100

    Update board function when size gets changed

--- my-app/src/board.jsx
+++ my-app/src/board.jsx
@@ -1,11 +1,20 @@
 import { Component } from "react";
 
 class Board extends Component {
+    constructor() {
+        super();
+    }
+
+    updateBoard(sizePercentage) {
+        console.log(this.state)
+    }
+
+
     render() {
         return (
-            <>
+            <div id="board">
 
-            </>
+            </div>
         );
     }
 }
