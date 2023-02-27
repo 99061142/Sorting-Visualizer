3e72d62e02b85c36fb379ba0e29a3d92b64a7e4a
99061142 <xarbprive@gmail.com>
2023-02-27 21:25:39 +0100

    Create cell with random height

--- my-app/src/cell.jsx
+++ my-app/src/cell.jsx
@@ -1,13 +1,41 @@
-import { Component } from "react";
+import { Component, createRef } from "react";
 
 class Cell extends Component {
     constructor() {
         super();
+        this.cell = createRef();
+        this.state = {
+            height: 0
+        }
+    }
+
+    setHeight(val) {
+        this.setState({
+            height: val
+        })
+    }
+
+    componentDidMount() {
+        // Get height position of the element
+        const CELL = this.cell.current;
+        let { y } = CELL.getBoundingClientRect();
+
+        // Get the max height of the website
+        const WINDOW_HEIGHT = window.innerHeight
+        const MAX_HEIGHT = WINDOW_HEIGHT - y;
+
+        // Set an random height to the cell lower than the max height
+        const RANDOM_HEIGHT = Math.floor(Math.random() * MAX_HEIGHT);
+        this.setHeight(RANDOM_HEIGHT);
     }
 
     render() {
+        const STYLING = {
+            'height': this.state.height,
+            'width': this.props.width
+        };
         return (
-            <div></div>
+            <div ref={this.cell} style={STYLING} className="bg-primary"></div>
         );
     }
 }
