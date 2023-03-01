6731054195a5638565917eb6e8ca53d0080388e1
99061142 <xarbprive@gmail.com>
2023-03-01 14:08:22 +0100

    Delete unnecessary code

--- my-app/src/cell.jsx
+++ my-app/src/cell.jsx
@@ -1,41 +1,13 @@
-import { Component, createRef } from "react";
+import { Component } from "react";
 
 class Cell extends Component {
-    constructor() {
-        super();
-        this.cell = createRef();
-        this.state = {
-            height: 0
-        }
-    }
-
-    setHeight(val) {
-        this.setState({
-            height: val
-        })
-    }
-
-    componentDidMount() {
-        // Get height position of the element
-        const CELL = this.cell.current;
-        let { y } = CELL.getBoundingClientRect();
-
-        // Get the max height of the website
-        const WINDOW_HEIGHT = window.innerHeight
-        const MAX_HEIGHT = WINDOW_HEIGHT - y;
-
-        // Set an random height to the cell lower than the max height
-        const RANDOM_HEIGHT = Math.floor(Math.random() * MAX_HEIGHT);
-        this.setHeight(RANDOM_HEIGHT);
-    }
-
     render() {
         const STYLING = {
-            'height': this.state.height,
+            'height': this.props.height,
             'width': this.props.width
         };
         return (
-            <div ref={this.cell} style={STYLING} className="bg-primary"></div>
+            <div ref={this.element} style={STYLING} className="bg-primary"></div>
         );
     }
 }
