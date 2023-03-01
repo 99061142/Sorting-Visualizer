c0f2e4a5103abb71966fd85e71c2b6e2ca003aaf
99061142 <xarbprive@gmail.com>
2023-03-01 14:08:54 +0100

    Create list with numbers that is equal to elements

--- my-app/src/board.jsx
+++ my-app/src/board.jsx
@@ -2,12 +2,31 @@ import Cell from "./cell";
 import { Component } from "react";
 
 class Board extends Component {
+    updateBoard() {
+        // Make a list of random numbers with the size of the boardSize state
+        const LENGTH = this.props.boardSize;
+        const NUMBERS = Array.from({ length: LENGTH }, () => Math.floor(Math.random() * 500));
+        this.props.setNumbers(NUMBERS);
+    }
+
+    componentDidMount() {
+        // When the window gets resized, update board list
+        window.addEventListener('resize', () => this.updateBoard());
+
+        // Add the initialized board list
+        this.updateBoard(this.props.boardSize);
+    }
+
     render() {
         const CELL_WIDTH = this.props.windowWidth / this.props.boardSize;
         return (
             <div className="d-flex justify-content-center">
-                {[...Array(this.props.boardSize)].map((_, i) =>
-                    <Cell width={CELL_WIDTH} key={i} />
+                {this.props.numbers.map((number, i) =>
+                    <Cell
+                        width={CELL_WIDTH}
+                        height={number}
+                        key={i}
+                    />
                 )}
             </div>
         );
