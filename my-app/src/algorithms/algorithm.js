f416251ffaebd71f8af1a2e4296d2f1747b6a211
99061142 <xarbprive@gmail.com>
2023-03-05 00:57:00 +0100

    Added/Updated algorithm functionality
    Added:
    
    * sleep. (based on the speed the user chose.)
    * getCells. (get every cell element inside the board.)
    * getNumber. (get the number with the index parameter.)
    
    Updated:
    * next/sorted/selected/standard. (now it changes the element className based on the function).
    
    * clearBoardExceptSorted. (Now it changes the element className based on the function call to the function"standard".

--- my-app/src/algorithms/algorithm.js
+++ my-app/src/algorithms/algorithm.js
@@ -1,42 +1,60 @@
 import { Component } from "react";
 
 class Algorithm extends Component {
-    getCell(index) {
-        const BOARD = this.props.board;
-        const CELL = BOARD[index]
-        return CELL
+    getCells() {
+        const BOARD = document.getElementById("board");
+        const CELLS = BOARD.children;
+        return CELLS
     }
     
-    next(index) {
-        const CELL = this.getCell(index);
-        CELL.name = "next"
+    getCell(index) {
+        const CELLS = this.getCells();
+        const CELL = CELLS.item(index);
+        return CELL
     }
 
-    selected(index) {
-        const CELL = this.getCell(index);
-        CELL.name = "selected"
+    getNumber(index) {
+        const BOARD = this.props.getNumbers();
+        const NUMBER = BOARD[index];
+        return NUMBER
     }
 
-    sorted(index) {
-        const CELL = this.getCell(index);
-        CELL.name = "sorted"
+    async next(index) {
+        const CELL_ELEMENT = this.getCell(index)
+        CELL_ELEMENT.className= "next";
+        await this.sleep();
+    }
+
+    async selected(index) {
+        const CELL_ELEMENT = this.getCell(index)
+        CELL_ELEMENT.className= "selected";
+        await this.sleep();
+    }
+
+    async sorted(index) {
+        const CELL_ELEMENT = this.getCell(index)
+        CELL_ELEMENT.className= "sorted";
+        await this.sleep();
+    }
+
+    standard(index) {
+        const CELL_ELEMENT = this.getCell(index)
+        CELL_ELEMENT.className= "standard";
     }
 
     clearBoardExceptSorted() {
-        const BOARD = this.props.board;
-        for(let [i, data] of BOARD.entries()) {
-            if(data.name !== "sorted") {
-                data.name = "";
+        const CELLS = [...this.getCells()];
+        for (let [i, cell] of CELLS.entries()) {
+            if(cell.className !== "sorted") {
+                this.standard(i);
             }
         }
     }
 
-    previousSmaller(index) {
-        const CURRENT_CELL = this.getCell(index);
-        const CURRENT_NUMBER = CURRENT_CELL.number;
-        const PREVIOUS_CELL = this.getCell(index-1);
-        const PREVIOUS_NUMBER = PREVIOUS_CELL.number;
-        return PREVIOUS_NUMBER < CURRENT_NUMBER
+    sleep() {
+        let ms = 100 - this.props.getSpeed();
+        if(ms == 0) { return }
+        return new Promise(resolve => setTimeout(resolve, ms));
     }
 }
 
