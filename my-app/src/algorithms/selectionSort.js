7a7b5644296df51f9700aa8ee991ce2788e0aa00
99061142 <xarbprive@gmail.com>
2023-03-05 00:57:53 +0100

    Added/Let the old selection sort algorithm work.
    Updated function calls / variable values to the current function / variable names.

--- my-app/src/algorithms/selectionSort.js
+++ my-app/src/algorithms/selectionSort.js
@@ -1,6 +1,40 @@
-class SelectionSort {
-    async run() {
+import Algorithm from "./algorithm";
 
+class SelectionSort extends Algorithm {
+    constructor(props) {
+        super(props);
+    }
+
+    async loop(current) {
+        await this.selected(current);
+
+        // Find the lowest cell number
+        let lowestNum = this.getNumber(current);
+        let lowestIndex = current;
+        for(let i = current+1; i < this.props.boardSize; i++) {
+            const NUMBER = this.getNumber(i);
+            if(NUMBER < lowestNum) {
+                await this.next(lowestIndex);
+                await this.selected(i);
+                lowestIndex = i;
+                lowestNum = NUMBER;
+            } else {
+                await this.next(i);
+            }
+        }
+        // If the lowest index isn't the same as the starting index, switch cells
+        if(current !== lowestIndex) {
+            this.props.switchNumbers(current, lowestIndex);
+        }
+        await this.sorted(current);
+    }
+    
+    async run() {
+        // For every number in the list
+        for(let i = 0; i < this.props.boardSize; i++) {
+            await this.loop(i); // Move lowest number to the left of the list
+            this.clearBoardExceptSorted(); // Clear board except elements that are already sorted
+        }
     }
 }
 
