f4dc9643e654563d7ef399be7debfa5a7e2a35e2
99061142 <xarbprive@gmail.com>
2023-03-12 21:00:35 +0100

    Delete unnecessary classname

--- src/cell.jsx
+++ src/cell.jsx
@@ -2,7 +2,7 @@ function Cell({ number }) {
     const BOARD_SIZE = document.getElementById("boardSize").value;
     const WIDTH = window.innerWidth / BOARD_SIZE;
     return (
-        <div style={{ width: WIDTH, height: number }} className="standard">
+        <div style={{ width: WIDTH, height: number }}>
             {WIDTH >= 40 && number >= 25 &&
                 <p className="text-center">
                     {number}
