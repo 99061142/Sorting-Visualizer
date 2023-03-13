079734fb77652c30d747b6db72eed62e971acfd5
99061142 <xarbprive@gmail.com>
2023-03-13 13:59:33 +0100

    Starting mergeSort class

---
+++ src/algorithms/mergeSort.js
@@ -0,0 +1,13 @@
+import Algorithm from "./algorithm";
+
+class MergeSort extends Algorithm {
+    constructor(props) {
+        super(props);
+    }
+    
+    async run() {
+
+    }
+}
+
+export default MergeSort;
