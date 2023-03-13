0c923f2102c8feb16f875c36206c4502c734bf5b
99061142 <xarbprive@gmail.com>
2023-03-13 09:59:37 +0100

    changed const 'board_size' to 'numbers_amount'

--- src/cell.jsx
+++ src/cell.jsx
@@ -1,6 +1,6 @@
 function Cell({ number }) {
-    const BOARD_SIZE = document.getElementById("boardSize").value;
-    const WIDTH = window.innerWidth / BOARD_SIZE;
+    const NUMBERS_AMOUNT = document.getElementById("numbersAmount").value;
+    const WIDTH = window.innerWidth / NUMBERS_AMOUNT;
     return (
         <div style={{ width: WIDTH, height: number }}>
             {WIDTH >= 40 && number >= 25 &&
