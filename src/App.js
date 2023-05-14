c2fff2444a4bd52375a7d142d8747b4fef6fb5d3
99061142 <xarbprive@gmail.com>
2023-05-14 17:44:40 +0200

    Read description
    CHANGED:
    * class component to function component.
    
    ADDED:
    * Boardsize state + (getter/setter)
    
    DELETED:
    * numbers/running state.
    * numbers getters/setters
    *  runing setter.

--- src/App.js
+++ src/App.js
@@ -1,50 +1,16 @@
 import './app.css';
-import { Component } from 'react';
+import { useState } from 'react';
 import Settings from './settings';
 import Board from './board';
 
-class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            numbers: [],
-            running: false
-        };
-    }
-
-    setRunning = bool => {
-        this.setState({
-            running: bool
-        });
-    }
-
-    setNumbers = numbers => {
-        this.setState({
-            numbers
-        });
-    }
-
-    getNumbers = () => { 
-        const NUMBERS = this.state.numbers;
-        return NUMBERS
-    }
-
-    render() {
-        return (
-            <>
-                <Settings 
-                    {...this.state} 
-                    setRunning={this.setRunning}
-                    setNumbers={this.setNumbers}
-                    getNumbers={this.getNumbers}
-                />
-                <Board
-                    {...this.state}
-                    setNumbers={this.setNumbers}
-                />
-            </>
-        );
-    }
+function App() {
+    const [boardSize, setBoardSize] = useState(0);
+    return (
+        <>
+            <Settings setBoardSize={setBoardSize} />
+            <Board boardSize={boardSize} />
+        </>
+    );
 }
 
 export default App;
