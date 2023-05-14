6dc43bfbc6718224074861043cd62e4b30e4d62e
99061142 <xarbprive@gmail.com>
2023-05-14 17:48:23 +0200

    Read description
    CHANGED:
    * Class component to function component.
    
    ADDED:
    * Element ref.
    * Number state (getter/setter).
    * Observer that changes the number state as the dataset 'number' when the dataset 'number' gets updated.
    * Background color is inside the element styling itself (before it was inside the CSS file).
    * width / index prop when the component gets created.
    
    DELETED:
    * Const that has the total amount of cells.
    * Const that calculated the max width of the cell.

--- src/cell.jsx
+++ src/cell.jsx
@@ -1,14 +1,42 @@
-function Cell({ number }) {
-    const NUMBERS_AMOUNT = document.getElementById("numbersAmount").value;
-    const WIDTH = window.innerWidth / NUMBERS_AMOUNT;
+import { createRef, useEffect, useState } from "react";
+
+function Cell({ width, index }) {
+    const element = createRef(null);
+    const [number, setNumber] = useState(0)
+
+    // Update the setNumber state when the dataset 'number' gets changed
+    useEffect(() => {
+        const observer = new MutationObserver((mutations) => {
+            for (const mutation of mutations) {
+                const DATASET_NUMBER = Number(mutation.target.dataset.number);
+                setNumber(DATASET_NUMBER)
+            }
+        });
+
+        observer.observe(element.current, {
+            attributeFilter: ['data-number'],
+        });
+    }, [element]);
+
     return (
-        <div style={{ width: WIDTH, height: number }}>
-            {WIDTH >= 40 && number >= 25 &&
-                <p className="text-center">
+        <div
+            ref={element}
+            id={'cell-' + index}
+            data-number={number}
+            style={{
+                backgroundColor: '#007bff',
+                width,
+                height: number
+            }}
+        >
+            {width >= 40 && number >= 20 &&
+                <p
+                    className='text-center mb-0'
+                >
                     {number}
                 </p>
             }
-        </div>
+        </div >
     );
 }
 
