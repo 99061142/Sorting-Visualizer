50a3f79a76fe24fbfaaf1eecf70ff2f5f5bb7eaf
99061142 <xarbprive@gmail.com>
2023-05-14 18:21:15 +0200

    Read description
    CHANGED:
    * When the numbers get swapped, swap the numbers on the board with the element className instead of the setNumbers state.
    
    ADDED:
    * This variable for all cell elements inside the board.
    * This variable for all numbers on the board.
    * This variable for the board size.
    
    DELETED:
    * All props that the component gets.
    * Get cells function (was changed to this variable).

--- src/algorithms/algorithm.js
+++ src/algorithms/algorithm.js
@@ -1,92 +1,112 @@
-import { Component } from "react";
+import { Component } from 'react';
 
 class Algorithm extends Component {
-    getCells() {
-        const BOARD = document.getElementById("board");
-        const CELLS = BOARD.children;
-        return CELLS
+    constructor() {
+        super();
+        this.cells = [...document.querySelectorAll("[id^='cell']")];
+        this.numbers = this.cells.map(cell => Number(cell.dataset.number));
+        this.boardSize = this.numbers.length;
     }
 
-    swapNumbers(indexOne, indexTwo) {
-        // indexOne = number of indexTwo, indexTwo = number of indexOne
-        const NUMBERS = this.props.getNumbers();
-        const TEMP = NUMBERS[indexOne];
-        const NUMBER_TWO = NUMBERS[indexTwo];
-        NUMBERS[indexOne] = NUMBER_TWO;
-        NUMBERS[indexTwo] = TEMP;
-        this.props.setNumbers(NUMBERS);
+    nextSmallest(i) {
+        const CURRENT_NUMBER = this.getNumber(i);
+        const NEXT_NUMBER = this.getNumber(i+1);
+
+        console.log(NEXT_NUMBER)
+        if(NEXT_NUMBER === undefined) {
+            return false
+        }
+        return NEXT_NUMBER < CURRENT_NUMBER
     }
 
-    
-    getCell(index) {
-        const CELLS = this.getCells();
-        const CELL = CELLS.item(index);
-        return CELL
+    previousSmallest(i) {
+        const CURRENT_NUMBER = this.getNumber(i);
+        const PREVIOUS_NUMBER = this.getNumber(i-1);
+
+        if(PREVIOUS_NUMBER === undefined) {
+            return false
+        }
+        return PREVIOUS_NUMBER < CURRENT_NUMBER
     }
 
-    getNumber(index) {
-        const BOARD = this.props.getNumbers();
-        const NUMBER = BOARD[index];
+    getCell(i) {
+        const ID = 'cell-' + i;
+        const CELL_ELEMENT = document.getElementById(ID);
+        return CELL_ELEMENT;
+    }
+
+    getNumber(i) {
+        const NUMBER = this.numbers[i];
         return NUMBER
     }
 
-    async next(index) {
-        const CELL_ELEMENT = this.getCell(index)
-        CELL_ELEMENT.className= "next";
+    setCellClassName(i, className) {
+        const CELL_ELEMENT = this.getCell(i);
+        CELL_ELEMENT.className = className;
+    }
+
+    async selected(i) {
+        this.setCellClassName(i, 'selected');
         await this.sleep();
     }
 
-    async selected(index) {
-        const CELL_ELEMENT = this.getCell(index)
-        CELL_ELEMENT.className= "selected";
+    async next(i) {
+        this.setCellClassName(i, 'next');
         await this.sleep();
     }
 
-    async sorted(index) {
-        const CELL_ELEMENT = this.getCell(index)
-        CELL_ELEMENT.className= "sorted";
+    async sorted(i) {
+        this.setCellClassName(i, 'sorted');
         await this.sleep();
     }
 
-    standard(index) {
-        const CELL_ELEMENT = this.getCell(index)
-        CELL_ELEMENT.className= '';
+    standard(i) {
+        this.setCellClassName(i, '');
     }
 
-    clearBoardExceptSorted() {
-        const CELLS = [...this.getCells()];
-        for (let [i, cell] of CELLS.entries()) {
-            if(cell.className !== "sorted") {
+    sleep() {
+        const SPEED_ELEMENT = document.getElementById('speed');
+        const CURRENT_SPEED = Number(SPEED_ELEMENT.value);
+        const MS = 100 - CURRENT_SPEED;
+        return new Promise(resolve => setTimeout(resolve, MS));
+    }
+
+    swapNumbers(indexOne, indexTwo) {
+        const NUMBERS = this.numbers;
+        const NUMBER_ONE = NUMBERS[indexOne];
+        const NUMBER_TWO = NUMBERS[indexTwo];
+        this.setNumber(indexTwo, NUMBER_ONE);
+        this.setNumber(indexOne, NUMBER_TWO);
+    }
+
+    setNumber(i, number) {
+        // Set number inside list
+        this.numbers[i] = number;
+
+        // Set number on the screen
+        const CELL = this.cells[i];
+        CELL.dataset.number = number;
+    }
+
+    async clearBoardExceptSorted() {
+        for (const [i, cell] of this.cells.entries()) { 
+            const TYPE = cell.className;    
+            if(TYPE !== 'sorted') {
                 this.standard(i);
             }
         }
     }
 
-    async boardSorted() {
-        const CELLS = [...this.getCells()];
-        for (let [i, cell] of CELLS.entries()) {
-            if(cell.className !== "sorted") {
-                await this.sorted(i)
-            }
+    async clearBoard() {
+        for (let i = 0; i < this.boardSize; i++) {  
+            this.standard(i);
         }
     }
 
-    nextSmallest(index) {
-        const CURRENT_NUMBER = this.getNumber(index);
-        const NEXT_NUMBER = this.getNumber(index+1);
-        return NEXT_NUMBER < CURRENT_NUMBER
-    }
-
-    previousSmallest(index) {
-        const CURRENT_NUMBER = this.getNumber(index);
-        const PREVIOUS_NUMBER = this.getNumber(index-1);
-        return PREVIOUS_NUMBER < CURRENT_NUMBER
-    }
-
-    sleep() {
-        let ms = 100 - this.props.getSpeed();
-        if(ms == 0) { return }
-        return new Promise(resolve => setTimeout(resolve, ms));
+    async setBoardSorted() {
+        for (let i = 0; i < this.boardSize; i++) {  
+            await this.sorted(i);
+        }
     }
 }
 
