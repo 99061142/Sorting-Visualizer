15b8772ae7d63c6eee54d9f4c8a711b70dd9b662
99061142 <xarbprive@gmail.com>
2023-05-14 17:52:56 +0200

    Read description
    CHANGED:
    * class component to function component.
    
    ADDED:
    * Randomize the board when the board is created.
    * Give the max width of the cell as prop when the cell gets created.
    * Give the index of the cell as prop when the cell gets created.
    * Import 'randomizeBoard' function to randomize the board when needed.
    
    DELETED:
    * NumbersAmount state
    * Setter for the numbersAmount
    * Function to randomize the board (inside the board component itself).
    * Function to clear the board (cells styling).
    * Window resize listener, and the function that clears the board / randomize the board when it happens.

--- src/board.jsx
+++ src/board.jsx
@@ -1,77 +1,27 @@
-import Cell from "./cell";
-import { Component } from "react";
+import { useEffect } from 'react';
+import Cell from './cell';
+import RandomizeBoard from './randomizeBoard';
 
-class Board extends Component {
-    constructor() {
-        super();
-        this.state = {
-            numbersAmount: Math.floor(window.innerWidth * .025)
-        }
-    }
+function Board({ boardSize }) {
+    // Randomize the board when the board size gets updated
+    useEffect(() => {
+        RandomizeBoard();
+    }, [boardSize]);
 
-    async setNumbersAmount(amount) {
-        this.setState({
-            numbersAmount: amount
-        });
-    }
-
-    updateBoard() {
-        // If the algorithm is running, return
-        if (this.props.running) { return }
-
-        // Get the max height of the board
-        const BOARD = document.getElementById("board");
-        const BOARD_TOP = BOARD.getBoundingClientRect().top;
-        const WINDOW_HEIGHT = window.innerHeight;
-        const MAX_HEIGHT = Math.floor(WINDOW_HEIGHT - BOARD_TOP);
-
-        // Create a list with numbers
-        let numbers = [];
-        for (let i = 0; i < this.state.numbersAmount; i++) {
-            const RANDOM_NUMBER = Math.floor(Math.random() * MAX_HEIGHT) + 1;
-            numbers.push(RANDOM_NUMBER);
-        }
-        // Set the numbers as current
-        this.props.setNumbers(numbers);
-    }
-
-    windowResized() {
-        // If the algorithm is running, return
-        if (this.props.running) { return }
-
-        this.clearBoard();
-        this.updateBoard();
-    }
-
-    componentDidMount() {
-        window.addEventListener('resize', () => this.windowResized());
-
-        this.updateBoard();
-    }
-
-    clearBoard() {
-        // Clear all styling of the board
-        const BOARD = document.getElementById("board");
-        const CELLS = BOARD.children;
-        for (const cell of CELLS) {
-            if (cell.className !== '') {
-                cell.className = '';
-            }
-        }
-    }
-
-    render() {
-        return (
-            <div id="board" className="d-flex justify-content-center">
-                {this.props.numbers.map((number, i) =>
-                    <Cell
-                        number={number}
-                        key={i}
-                    />
-                )}
-            </div>
-        );
-    }
+    return (
+        <div
+            id='board'
+            className='d-flex justify-content-center'
+        >
+            {[...Array(boardSize)].map((_, key) =>
+                <Cell
+                    width={window.innerWidth / boardSize}
+                    index={key}
+                    key={key}
+                />
+            )}
+        </div>
+    );
 }
 
 export default Board;
